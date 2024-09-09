05231831eb2152dfccbceb7866a6ed34b44156e7
mateo-swapps <mbarrera@swapps.com>
2024-09-09 09:11:44 -0500

    project creation with Cookiecutter

---
+++ django_example/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from django_example.users.models import User
+from django_example.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
