282bd2ee4232b4775e7dd583c4e5f37a4fd77e9a
mateo-swapps <mbarrera@swapps.com>
2024-09-15 18:26:53 -0500

    docs: class base views added

--- polls/views.py
+++ polls/views.py
@@ -1,57 +1,78 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
 from django.utils import timezone
+from django.views import generic
 
 from .models import Choice, Question
 
 
 class IndexView(generic.ListView):
+    """
+    IndexView displays a list of the latest published questions.
+
+    Attributes:
+        template_name (str): The name of the template to use for rendering the view.
+        context_object_name (str): The name of the context variable to use in the template.
+    """
+
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
+        """
+        Returns the last five published questions.
+
+        Filters the questions to include only those with a publication date less than or equal to the current time,
+        and orders them by publication date in descending order.
+
+        Returns:
+            QuerySet: A queryset containing the last five published questions.
+        """
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
             "-pub_date"
         )[:5]
 
 
 class DetailView(generic.DetailView):
+    """
+    DetailView displays the details of a specific question.
+
+    Attributes:
+        model (Model): The model to use for the view.
+        template_name (str): The name of the template to use for rendering the view.
+    """
+
     model = Question
     template_name = "polls/detail.html"
 
     def get_queryset(self):
         """
         Excludes any questions that aren't published yet.
+
+        Filters the questions to include only those with a publication date less than or equal to the current time.
+
+        Returns:
+            QuerySet: A queryset containing the published questions.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
+    """
+    ResultsView displays the results of a specific question.
+
+    Attributes:
+        model (Model): The model to use for the view.
+        template_name (str): The name of the template to use for rendering the view.
+    """
+
     model = Question
     template_name = "polls/results.html"
 
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(
-            request,
-            "polls/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
-        )
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+class VoteView(generic.View):
+    """
+    VoteView handles the voting process for a specific question.
+
+    This class is a placeholder for the voting logic, which should be implemented in the future.
+    """
+
+    pass
